fda0665ad84a6c14698c0a9e3caa6c0684084879
Martin Deiss <deiss@gardenofconcepts.comn>
2020-01-13 18:10:12 +0100

    fixed dirfix.sh

--- docker/grafana/dirFix.sh
+++ docker/grafana/dirFix.sh
@@ -6,5 +6,5 @@ echo "Create folders for Grafana and fix permissions"
 [ -d ./volumes/grafana/data ] || mkdir -p ./volumes/grafana/data
 [ -d ./volumes/grafana/log ] || mkdir -p ./volumes/grafana/log
 
-sudo chown -R 472:472 ./volumesgrafana/data/ 
-sudo chown -R 472:472 ./volumesgrafana/log/ 
+sudo chown -R 472:472 ./volumes/grafana/data/ 
+sudo chown -R 472:472 ./volumes/grafana/log/
