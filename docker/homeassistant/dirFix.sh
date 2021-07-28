afc435fee4e8f9cb051905658a4a261e7f265951
Martin Deiss <deiss@gardenofconcepts.com>
2021-07-29 00:56:36 +0200

    downgraded homeassistant, updated homeassistant-docker-venv

--- docker/homeassistant/dirFix.sh
+++ docker/homeassistant/dirFix.sh
@@ -6,4 +6,4 @@ echo "Create folders for HomeAssistant "
 [ -d ./volumes/homeassistant/config ] || mkdir -p ./volumes/homeassistant/config
 
 # Run Home Assistant as non-root using the official docker image
-git clone --branch v2.1 https://github.com/tribut/homeassistant-docker-venv ./volumes/homeassistant/docker
+git clone --branch v2.3 https://github.com/tribut/homeassistant-docker-venv ./volumes/homeassistant/docker
